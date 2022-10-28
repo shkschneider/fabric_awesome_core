c2710b3ddf9e4ca4db9952cebb290abdbd9f280f
shkschneider <shk.schneider@gmail.com>
2022-10-28 18:27:34 +0200

    BowItem mixin for proper Infinity

---
+++ src/main/java/io/github/shkschneider/awesome/mixins/BowItemArrowCountMixin.java
@@ -0,0 +1,29 @@
+package io.github.shkschneider.awesome.mixins;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+// https://github.com/ClusterFluxMC/extension
+@Mixin(BowItem.class)
+public class BowItemArrowCountMixin {
+
+    @Inject(method="use(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/TypedActionResult;", at=@At("HEAD"), cancellable=true)
+    private void arrowCount(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+        final ItemStack stack = user.getStackInHand(hand);
+        if (EnchantmentHelper.getLevel(Enchantments.INFINITY, stack) > 0) {
+            user.setCurrentHand(hand);
+            cir.setReturnValue(TypedActionResult.pass(stack));
+        }
+    }
+
+}
