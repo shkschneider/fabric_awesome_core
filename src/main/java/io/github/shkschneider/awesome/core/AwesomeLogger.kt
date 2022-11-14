43068c03008bfb60ea5e29f08c74fb3df55e3182
shkschneider <shk.schneider@gmail.com>
2022-11-14 22:49:13 +0100

    AwesomeLogger with proper tag

--- src/main/java/io/github/shkschneider/awesome/core/AwesomeLogger.kt
+++ src/main/java/io/github/shkschneider/awesome/core/AwesomeLogger.kt
@@ -1,26 +1,33 @@
 package io.github.shkschneider.awesome.core
 
 import io.github.shkschneider.awesome.Awesome
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 object AwesomeLogger {
 
-    private val TAG = Awesome.NAME
+    private val logger = LoggerFactory.getLogger(Awesome::class.java)
 
-    private val logger: Logger = LoggerFactory.getLogger(TAG)
-
-    fun trace(throwable: Throwable) = logger.trace(throwable.toString()).also { throwable.printStackTrace() }
-
+    @Deprecated("Not printing using logger.debug().")
     fun debug(msg: String) {
-        // Minecraft has isDevelopment always false in my environment
-        if (Minecraft.isDevelopment) println("[$TAG] $msg")
+        if (Minecraft.isDevelopment) println(msg)
     }
 
-    fun info(msg: String) = logger.info(msg)
+    fun info(msg: String) {
+        logger.info(msg)
+    }
 
-    fun warn(msg: String) = logger.warn(msg)
+    fun warn(msg: String) {
+        logger.warn(msg)
+    }
 
-    fun error(msg: String) = logger.error(msg)
+    fun error(msg: String) {
+        logger.error(msg)
+    }
+
+    fun trace(throwable: Throwable) {
+        logger.trace(throwable.toString())
+        throwable.printStackTrace()
+    }
+
 
 }
