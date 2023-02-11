c624e5726d1f4b90abd3edeb0cd46ac30a83b922
shkschneider <shk.schneider@gmail.com>
2023-02-11 13:15:38 +0100

    Extras: new gamerules overrides (optionnal, off by default)

--- extras/src/main/kotlin/io/github/shkschneider/awesome/extras/GameRulesOverrides.kt
+++ extras/src/main/kotlin/io/github/shkschneider/awesome/extras/GameRulesOverrides.kt
@@ -1,6 +1,7 @@
 package io.github.shkschneider.awesome.extras
 
 import io.github.shkschneider.awesome.custom.Event
+import io.github.shkschneider.awesome.custom.Minecraft
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents
 import net.minecraft.world.GameRules
 
@@ -9,11 +10,14 @@ object GameRulesOverrides {
     operator fun invoke() {
         @Event("ServerWorldEvents.Load")
         ServerWorldEvents.LOAD.register(ServerWorldEvents.Load { server, world ->
-            world.gameRules.get(GameRules.DO_FIRE_TICK).set(false, server)
-            world.gameRules.get(GameRules.DO_MOB_GRIEFING).set(false, server)
             world.gameRules.get(GameRules.DO_DAYLIGHT_CYCLE).set(true, server)
+            world.gameRules.get(GameRules.DO_FIRE_TICK).set(false, server)
             world.gameRules.get(GameRules.DO_IMMEDIATE_RESPAWN).set(false, server)
+            world.gameRules.get(GameRules.DO_MOB_GRIEFING).set(false, server)
+            world.gameRules.get(GameRules.LAVA_SOURCE_CONVERSION).set(true, server)
             world.gameRules.get(GameRules.PLAYERS_SLEEPING_PERCENTAGE).set(50, server)
+            world.gameRules.get(GameRules.REDUCED_DEBUG_INFO).set(Minecraft.isDevelopment.not(), server)
+            world.gameRules.get(GameRules.WATER_SOURCE_CONVERSION).set(true, server)
         })
     }
 
