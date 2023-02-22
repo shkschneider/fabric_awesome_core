f19b55db3d5e1b7bae1ee3439bd9c361aa86f544
shkschneider <shk.schneider@gmail.com>
2023-02-22 12:01:52 +0100

    Core: more colors

--- core/src/main/kotlin/io/github/shkschneider/awesome/core/AwesomeColors.kt
+++ core/src/main/kotlin/io/github/shkschneider/awesome/core/AwesomeColors.kt
@@ -7,29 +7,41 @@ package io.github.shkschneider.awesome.core
 @Suppress("MayBeConstant") // do not const val because of java accessors
 object AwesomeColors {
 
-    val black = 0x000000
-    val blue = 0x0000FF
+    // https://github.com/sp614x/optifine/blob/master/OptiFineDoc/doc/color.properties
+    val black = 0x1d1d21
+    val blue = 0x3c44aa
+    val blueLight = 0x3ab3da
+    val brown = 0x835432
     val charcoal = 0x434343
     val coal = 0x464646
-    val copper = 0xFF6400
-    val diamond = 0xC8FFFF
-    val emerald = 0x50FF50
+    val copper = 0xff6400
+    val cyan = 0x169c9c
+    val diamond = 0xc8ffff
+    val emerald = 0x50ff50
     val ender = 0x258273
-    val gold = 0xFFFF1E
-    val green = 0x00ff00
-    val iron = 0xC8C8C8
-    val lapis = 0x4646DC
+    val gold = 0xffff1e
+    val gray = 0x474f52
+    val grayLight = 0x9d9d97
+    val green = 0x5e7c16
+    val iron = 0xc8c8c8
+    val lapis = 0x4646dc
     val lilyPadBlock = 0x208030
-    val lilyPadItem = 0x71C35C
+    val lilyPadItem = 0x71c35c
+    val lime = 0x80c71f
+    val magenta = 0xc74ebd
     val mischka = 0xacadb2
-    val mojang = 0xDB1F29
-    val netherite = 0x4B484B
-    val quartz = 0xE6D2D2
-    val randomium = 0xEC23FF
-    val red = 0xFF0000
-    val redstone = 0xC80000
-    val tuna = 0x48494D
-    val valencia = 0xd84C45
-    val white = 0xFFFFFF
+    val mojang = 0xdb1f29
+    val netherite = 0x4b484b
+    val orange = 0xf9801d
+    val pink = 0xf38baa
+    val purple = 0x8932b8
+    val quartz = 0xe6d2d2
+    val randomium = 0xec23ff
+    val red = 0xb02e26
+    val redstone = 0xc80000
+    val tuna = 0x48494d
+    val valencia = 0xd84c45
+    val white = 0xf9fffe
+    val yellow = 0xfed83d
 
 }
